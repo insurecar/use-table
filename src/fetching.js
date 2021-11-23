f9df6cd3d6040f6821ad893149e7454cb5c98f29
rostyslav <rostyslavshyian@gmail.com>
2021-11-23 10:52:19 +0200

    added api length

--- src/fetching.js
+++ src/fetching.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetch = (count) =>
   axios.get(
-    `https://618cfaa2edab980017fd512c.mockapi.io/duna-order?page=1&limit=${count}`
+    `https://618cfaa2edab980017fd512c.mockapi.io/duna-order?page=1&limit=${1}`
   );
 
 export const fetchLength = () =>
